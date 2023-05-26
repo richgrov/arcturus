80c51b0b451642b0d8ff3a30de10457614e3feb3
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-05-25 21:39:03 -0600

    Add nav bar

---
+++ src/routes/(authed)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { getAuth, signOut } from 'firebase/auth';
+
+  async function logout(event: any) {
+    const auth = getAuth();
+    await signOut(auth);
+    goto(event.target.href);
+  }
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/login" on:click|preventDefault={logout}>Logout</a>
+</nav>
+
+<slot />
