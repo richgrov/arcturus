3703743dd54367b9d409131f85b54f3bb15b79a5
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-06-17 21:16:32 -0600

    Fix missing default tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,17 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: "#ff4141",
-      foreground: "#111",
-      "foreground-dark": "#eee",
-      background: "#eee",
-      "background-dark": "#111",
-    },
     extend: {
       backgroundImage: {
         login:
           "linear-gradient(to bottom, black, transparent), url('background.png')",
+      },
+      colors: {
+        primary: "#ff4141",
+        foreground: "#111",
+        "foreground-dark": "#eee",
+        background: "#eee",
+        "background-dark": "#111",
       },
     },
   },
